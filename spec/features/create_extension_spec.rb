e79c7c61aaea7bcc56777dfd1809b10f929c09cc
Mattia Roccoberton <mat@blocknot.es>
2019-12-11 14:22:19 +0100

    Use FileUtils to improve readability of shell commands

--- spec/features/create_extension_spec.rb
+++ spec/features/create_extension_spec.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require 'fileutils'
 require 'spec_helper'
 
 RSpec.describe 'Create extension' do # rubocop:disable Metrics/BlockLength
+  include FileUtils
+
   let(:ext_root) { File.expand_path('../..', __dir__) }
   let(:solidus_cmd) { "#{ext_root}/exe/solidus" }
   let(:extension_name) { 'test_extension' }
@@ -12,9 +15,9 @@ RSpec.describe 'Create extension' do # rubocop:disable Metrics/BlockLength
   let(:command_failed) { Class.new(StandardError) }
 
   around do |example|
-    tmp_path.mkpath
+    mkdir_p(tmp_path)
     example.run
-    tmp_path.rmtree
+    rm_rf(tmp_path)
   end
 
   def check_solidus_cmd
@@ -24,25 +27,28 @@ RSpec.describe 'Create extension' do # rubocop:disable Metrics/BlockLength
   end
 
   def check_create_extension
-    output = `cd #{tmp_path} && #{solidus_cmd} extension #{extension_name}`
+    output = cd(tmp_path) { `#{solidus_cmd} extension #{extension_name}` }
     expect($?).to be_success
     expect(output).to include(gemspec_name)
     expect(output).to include('.circleci')
   end
 
   def check_bundle_install
-    expect { sh("cd #{install_path} && bundle install 2>&1") }.to(
-      raise_error(command_failed, /invalid gemspec/)
-    )
+    expect { cd(install_path) { sh('bundle install 2>&1') } }.to raise_error(command_failed, /invalid gemspec/)
     # Update gemspec with the required fields
     gemspec_path = install_path.join(gemspec_name)
     new_content = gemspec_path.read.gsub(/\n.*s.author[^\n]+/, "\n  s.author = 'someone'").gsub(/TODO/, 'something')
     gemspec_path.write(new_content)
-    expect(sh("cd #{install_path} && bundle install 2>&1")).to include('Bundle complete!')
+    output = cd(install_path) do
+      sh('bundle install 2>&1')
+    end
+    expect(output).to include('Bundle complete!')
   end
 
   def check_default_task
-    output = sh("cd #{install_path} && bundle exec rake 2>&1")
+    output = cd(install_path) do
+      sh('bundle exec rake 2>&1')
+    end
     expect(output).to include('Generating dummy Rails application')
     expect(output).to include('no offenses detected')
     expect(output).to include('0 examples, 0 failures')
@@ -52,7 +58,9 @@ RSpec.describe 'Create extension' do # rubocop:disable Metrics/BlockLength
     install_path.join('spec', 'some_spec.rb').write(
       "require 'spec_helper'\nRSpec.describe 'Some test' do it { expect(true).to be_truthy } end\n"
     )
-    output = sh("cd #{install_path} && bundle exec rspec 2>&1")
+    output = cd(install_path) do
+      sh('bundle exec rspec 2>&1')
+    end
     expect(output).to include('1 example, 0 failures')
     expect(output).to include('Coverage report generated')
   end
