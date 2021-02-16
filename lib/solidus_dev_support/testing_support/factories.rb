68b6496c80dee4f8ecc24b982a3fd01a120c6316
Alberto Vena <kennyadsl@gmail.com>
2021-02-16 17:16:31 +0100

    Fix loading factories in extensions after the last changes in Solidus
    This commit reflects the last changes we made in Solidus to
    support the new FactoryBot loading mechanism.
    
    When testing extensions using Solidus vesions that support
    the definition_file_paths, it will load the core factory first
    and then the ones defined in
    `lib/extension_name/testing_support/factories`.

--- lib/solidus_dev_support/testing_support/factories.rb
+++ lib/solidus_dev_support/testing_support/factories.rb
@@ -1,26 +1,50 @@
 # frozen_string_literal: true
 
-require 'spree/testing_support/factories'
-require 'factory_bot'
+begin
+  require 'spree/testing_support/factory_bot'
+rescue LoadError
+  require 'factory_bot'
+  require 'spree/testing_support/factories'
+end
 
 module SolidusDevSupport
   module TestingSupport
     module Factories
       def self.load_for(*engines)
         paths = engines.flat_map do |engine|
-          engine.root.glob('lib/**/factories.rb')
+          factories_file_or_folder = engine.root.glob('lib/*/testing_support/factories{,.rb}')
+
+          if factories_file_or_folder.size == 2 && using_factory_bot_definition_file_paths?
+            folder, file = factories_file_or_folder.partition(&:directory?).map(&:first).map { |path| path.to_s.gsub(engine.root.to_s, '') }
+            ActiveSupport::Deprecation.warn <<-WARN.squish, caller(4)
+              SolidusDevSupport::TestingSupport::Factories.load_for() is automatically loading
+              all factories present into #{folder}. You should now safely remove #{file} if it
+              is only used to load ./factories content.
+            WARN
+
+            engine.root.glob('lib/*/testing_support/factories/**/*_factory.rb')
+          else
+            factories_file_or_folder
+          end.map { |path| path.sub(/.rb\z/, '').to_s }
         end
 
-        if Spree::TestingSupport.respond_to? :load_all_factories
-          FactoryBot.definition_file_paths.concat(
-            paths.map { |path| path.sub(/.rb\z/, '').to_s }
-          )
+        if using_factory_bot_definition_file_paths?
+          FactoryBot.definition_file_paths = [
+            Spree::TestingSupport::FactoryBot.definition_file_paths,
+            paths,
+          ].flatten
+
           FactoryBot.reload
         else
           FactoryBot.find_definitions
 
           paths.each { |path| require path }
         end
+      end
+
+      def self.using_factory_bot_definition_file_paths?
+        defined?(Spree::TestingSupport::FactoryBot) &&
+          Spree::TestingSupport::FactoryBot.respond_to?(:definition_file_paths)
       end
     end
   end
