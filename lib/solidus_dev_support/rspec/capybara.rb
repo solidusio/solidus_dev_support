c8745d3e72094a79a6c70e740420e43a11624de8
Thomas von Deyen <thomas@vondeyen.com>
2025-04-11 11:55:23 +0200

    Pin headless chrome to 133
    Unless the capybara team figured out how to solve this bug:
    
    https://github.com/teamcapybara/capybara/issues/2800
    
    Also setting some flags we need for Chrome 133 support.
    
    And completely register a new driver. Do not try to inherit from the selenium web driver. It does not contain all the args we need.

--- lib/solidus_dev_support/rspec/capybara.rb
+++ lib/solidus_dev_support/rspec/capybara.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
+require "selenium-webdriver"
+
 # Allow to override the initial windows size
-CAPYBARA_WINDOW_SIZE = ENV.fetch('CAPYBARA_WINDOW_SIZE', '1920x1080').split('x', 2).map(&:to_i)
-CAPYBARA_WINDOW_WIDTH = CAPYBARA_WINDOW_SIZE[0]
-CAPYBARA_WINDOW_HEIGHT = CAPYBARA_WINDOW_SIZE[1]
+CAPYBARA_WINDOW_SIZE = ENV.fetch('CAPYBARA_WINDOW_SIZE', '1920x1080')
+
+# Set Chrome version you want to use
+CAPYBARA_JAVASCRIPT_DRIVER_VERSION = ENV.fetch('CAPYBARA_JAVASCRIPT_DRIVER_VERSION', "133")
 
 Capybara.javascript_driver = ENV.fetch('CAPYBARA_JAVASCRIPT_DRIVER', "solidus_chrome_headless").to_sym
 Capybara.default_max_wait_time = 10
 Capybara.server = :puma, { Silent: true } # A fix for rspec/rspec-rails#1897
 
-Capybara.drivers[:selenium_chrome_headless].tap do |original_driver|
-  Capybara.register_driver :solidus_chrome_headless do |app|
-    original_driver.call(app).tap do |driver|
-      driver.resize_window_to(
-        driver.current_window_handle, CAPYBARA_WINDOW_WIDTH, CAPYBARA_WINDOW_HEIGHT
-      )
-    end
+Capybara.register_driver :solidus_chrome_headless do |app|
+  browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument("--headless=new")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--window-size=#{CAPYBARA_WINDOW_SIZE}")
+    opts.add_argument("--disable-search-engine-choice-screen")
+    opts.add_argument("--disable-backgrounding-occluded-windows")
+    opts.browser_version = CAPYBARA_JAVASCRIPT_DRIVER_VERSION
   end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
 require 'capybara-screenshot/rspec'
