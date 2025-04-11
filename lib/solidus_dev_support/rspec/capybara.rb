5884cdec73d32a969c31ae7f414442df8ac927f8
Thomas von Deyen <thomas@vondeyen.com>
2025-04-11 18:12:04 +0200

    Add more chrome flags
    Found in ref: https://github.com/teamcapybara/capybara/issues/2796

--- lib/solidus_dev_support/rspec/capybara.rb
+++ lib/solidus_dev_support/rspec/capybara.rb
@@ -21,7 +21,20 @@ Capybara.register_driver :solidus_chrome_headless do |app|
     opts.add_argument("--no-sandbox")
     opts.add_argument("--window-size=#{CAPYBARA_WINDOW_SIZE}")
     opts.add_argument("--disable-search-engine-choice-screen")
-    opts.add_argument("--disable-backgrounding-occluded-windows")
+
+    # From https://github.com/teamcapybara/capybara/issues/2796
+    # Chrome flags found from:
+    # - https://peter.sh/experiments/chromium-command-line-switches/
+    # - https://github.com/GoogleChrome/chrome-launcher/blob/main/docs/chrome-flags-for-tools.md
+    #
+    # Disable timers being throttled in background pages/tabs. Useful for parallel test runs.
+    opts.add_argument("disable-background-timer-throttling")
+    # Normally, Chrome will treat a "foreground" tab instead as backgrounded if the surrounding window is occluded (aka
+    # visually covered) by another window. This flag disables that. Useful for parallel test runs.
+    opts.add_argument("disable-backgrounding-occluded-windows")
+    # This disables non-foreground tabs from getting a lower process priority. Useful for parallel test runs.
+    opts.add_argument("disable-renderer-backgrounding")
+
     opts.browser_version = CAPYBARA_JAVASCRIPT_DRIVER_VERSION
   end
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
